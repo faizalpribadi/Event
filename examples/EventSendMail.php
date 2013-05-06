12ae9f672ead3a8ca4fb2b5e2af6dff610d16ef5
FaizalPribadi <faizal_pribadi@aol.com>
2013-05-06 23:34:53 +0700

    examples to use

---
+++ examples/EventSendMail.php
@@ -0,0 +1,109 @@
+<?php
+$fileLoader = dirname(dirname(__FILE__));
+require $fileLoader . '/autoload.php';
+
+use Mozart\Library\Event\EventDispatcher;
+use Mozart\Library\Event\Event;
+
+/**
+ * Create own your event name "custom"
+ */
+final class SendMailer
+{
+    /**
+     * Const custom event name
+     */
+    const EVENT_SENDMAIL = 'event.sendmail';
+}
+
+/**
+ * Extended the Mozart\Library\Event\Event
+ * And create custom event configuration to render
+ */
+class SendMail extends Event
+{
+    /**
+     * @var null
+     */
+    protected $mail = null;
+
+    /**
+     * @param $mail
+     *
+     * @return $this
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+    }
+
+    /**
+     * @return from $this->setMail()
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __toString()
+    {
+        return $this->getMail();
+    }
+}
+
+/**
+ * Create the custom configuration event listener
+ */
+class SendMailListener
+{
+    /**
+     * @param SendMail $sendMail
+     *
+     * @return mixed
+     */
+    public function onSend(SendMail $sendMail)
+    {
+        return $sendMail->setMail('this is mail content')->getMail();
+    }
+}
+
+/**
+ * Class SendMailEvent
+ */
+class SendMailEvent
+{
+    /**
+     * @var Mozart\Library\Event\EventDispatcher
+     */
+    protected $dispatcher;
+
+    /**
+     * @param EventDispatcher $dispatcher
+     */
+    public function __construct(EventDispatcher $dispatcher = null)
+    {
+        if (null === $dispatcher) {
+            $dispatcher = new EventDispatcher();
+            $this->dispatcher = $dispatcher;
+        }
+
+        $listener = new SendMailListener();
+        $this->dispatcher->addListener(SendMailer::EVENT_SENDMAIL, array($listener, 'onSend'));
+    }
+
+    /**
+     * @return mixed|Event
+     */
+    public function send()
+    {
+        $event = new SendMail();
+
+        return $this->dispatcher->dispatch(SendMailer::EVENT_SENDMAIL, $event);
+    }
+}
+
+$sendMail = new SendMailEvent();
+echo $sendMail->send();
