3163f98d9f2afc243bca3e9f2d92cee21e2e17c1
FaizalPribadi <faizal_pribadi@aol.com>
2013-05-09 11:30:23 +0700

    error extending

---
+++ Exception/WrongArgumentException.php
@@ -0,0 +1,22 @@
+<?php
+namespace Mozart\Library\Event\Exception;
+
+/*
+ * This file is a part of Mozart PHP Small MVC Framework
+ *
+ * (c) Faizal Pribadi <faizal_pribadi@aol.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ *
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Class WrongArgumentException
+ *
+ * @author  Faizal Pribadi <faizal_pribadi@aol.com>
+ */
+class WrongArgumentException extends \InvalidArgumentException
+{
+
+}
