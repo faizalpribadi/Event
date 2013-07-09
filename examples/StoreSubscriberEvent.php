063e1fb3e535f5b2fc9d54db36b25c858beb8ffa
FaizalPribadi <faizal_pribadi@aol.com>
2013-07-10 03:00:05 +0700

    subscribers array | string

--- examples/StoreSubscriberEvent.php
+++ examples/StoreSubscriberEvent.php
@@ -2,31 +2,54 @@
 namespace Mozart\Library\Event\examples;
 
 use Mozart\Library\Event\Event;
-use Mozart\Library\Event\EventDispatcher;
 use Mozart\Library\Event\Subscriber\SubscriberInterface;
 
-final class StoreEvent
+class StoreSubscriberEvent
 {
-    const STORE_EVENT = 'store.event';
+    public function render()
+    {
+        return 'shopping';
+    }
+}
+
+class FilterStoreEvent extends Event
+{
+    protected $store;
+
+    public function setStore(Store $store)
+    {
+        $this->store = $store;
+    }
+
+    public function getStore()
+    {
+        return $this->store;
+    }
 }
 
 class StoreSubscriberEvent implements SubscriberInterface
 {
 
+    /**
+     * Get all method on event configuration
+     *
+     * @return mixed
+     *
+     * @api
+     */
     public function getSubscriberEvents()
     {
+        // TODO: Implement getSubscriberEvents() method.
         return array(
-            'Subscribers' => 'onStoreSomething'
+
+            // subscribers
+             'subscribers' => 'onStore',
+             'event.all' => array('onStore')
         );
     }
 
-    public function onStoreSomething(Event $event)
+    public function onStore(FilterStoreEvent $event)
     {
-        return $event->setEvent(StoreEvent::STORE_EVENT);
+
     }
 }
-
-$dispatcher = new EventDispatcher();
-$event = new \StoreSubscriberEvent();
-$dispatcher->addSubscriber($event);
-$dispatcher->dispatch('Subscribers');
